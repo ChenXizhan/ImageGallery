e0a2f973d9af72d1597ff6c9af15f33595c3ff9b
ChenXizhan <chenxizhan666@163.com>
2016-12-04 00:22:18 +0800

    version 1
    这个才是真正的version 1， 前面那个提交信息放在这一个提交上正合适

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,24 +1,36 @@
-function showPic(whichpic){
-	var source = whichpic.getAttribute("href");
-	var placeholder = document.getElementById("placeholder");
-	var text = whichpic.getAttribute("title");
-	var description = document.getElementById("description");
-	placeholder.setAttribute("src", source)
-	description.firstChild.nodeValue=text;
 
+function showPic(whichpic){
+
+	var source = whichpic.getAttribute("href");
+	if (!source) return false;
+	var placeholder = document.getElementById("placeholder");
+	if (!placeholder) return false;
+	placeholder.setAttribute("src", source)
+
+	var description = document.getElementById("description");
+	if (description){
+		var text = whichpic.getAttribute("title");
+		if (!text)  text="";
+		description.firstChild.nodeValue=text;
+	}
+
+	return true;
 }
 
 
 function prepareLinks(){
-	var links = document.getElementsByTagName("a");
-	for (var i=0;i<links.length;i++){
-		if(links[i].getAttribute("class") == "popup"){
-			links[i].onclick=function(){
-				showPic(this);
-				return false;
-			}
-		}
+	if (!document.getElementById) return false;
+	var gallery = document.getElementById("image-gallery");
+	if (!gallery) return false;
 
+	var links = gallery.getElementsByTagName("a");
+	for (var i = 0; i<links.length; i++){
+		links[i].onclick=function(){
+			if(showPic(this)){
+				// 如果图片切换成功，拦截链接跳转的默认行为
+				return false;
+			}else{return true;}
+		}
 	}
 }
 
